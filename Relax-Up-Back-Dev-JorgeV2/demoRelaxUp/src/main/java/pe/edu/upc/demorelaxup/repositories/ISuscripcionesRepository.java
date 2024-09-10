3d5e3c9a0058327c70468b34b5edaaade0ebb9ec
Casof46 <115044167+Casof46@users.noreply.github.com>
2024-09-09 21:55:45 -0500

    Delete Relax-Up-Back-Dev-JorgeV2 directory

--- Relax-Up-Back-Dev-JorgeV2/demoRelaxUp/src/main/java/pe/edu/upc/demorelaxup/repositories/ISuscripcionesRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.demorelaxup.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.demorelaxup.entities.Suscripciones;
-@Repository
-public interface ISuscripcionesRepository extends JpaRepository<Suscripciones, Integer> {
-}
