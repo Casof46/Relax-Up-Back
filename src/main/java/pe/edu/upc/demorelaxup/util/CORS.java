461819be924d860d9b45a3ef51e8cbf37aa863fd
joaquinSolano <Jsolanoupc04@gmail.com>
2024-11-21 00:03:23 -0500

    deploy

--- src/main/java/pe/edu/upc/demorelaxup/util/CORS.java
+++ src/main/java/pe/edu/upc/demorelaxup/util/CORS.java
@@ -26,6 +26,42 @@ public class CORS implements Filter {
 
 		// Especifica la URL de tu frontend aquí
 		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/usuarios");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/usuarios/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/rol");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/rol/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/eventos");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/eventos/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/foross");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/foross/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/mensajeforos");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/mensajeforos/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/rutina");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/rutina/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/UsuarioRutina");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/UsuarioRutina/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/ejerciciorutinas");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/ejerciciorutinas/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/emergencias");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/emergencias/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/tecnicasrelajacion");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/tecnicasrelajacion/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/planes");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/planes/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/metodos");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/metodos/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/suscripciones");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/suscripciones/nuevo");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/gatos");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes/totalingresos");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes/emergencias");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes/reporte-confirmaron");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes/reporte-progreso");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes/suscripciones");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes/Forosporusuario");
+		response.setHeader("Access-Control-Allow-Origin", "https://relaxupfrontenddeploy.vercel.app/reportes/Mensajeporforo");
+
 		response.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
